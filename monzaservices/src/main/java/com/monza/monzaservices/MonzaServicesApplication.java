9dd128b4e6546bfe304b382dfaf81ac61b7d55b0
Sekhar Pasem <sekhar.monza@gmail.com>
2017-10-22 14:47:43 +0530

    First Commit

---
+++ monzaservices/src/main/java/com/monza/monzaservices/MonzaServicesApplication.java
@@ -0,0 +1,12 @@
+package com.monza.monzaservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonzaServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonzaServicesApplication.class, args);
+	}
+}
